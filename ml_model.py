b62fb07ae6c6ec51d7bfefd074bfc6674ccf38e9
nikkithought <nikkithought@gmail.com>
2022-03-22 12:09:39 +0530

    Added multiple models

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,12 @@
 
 
 print("Adding all ML Code here")
+
+def model1():
+    pirnt("Adding code of ML Model 1")
+
+def model2():
+    pirnt("Adding code of ML Model 2")
+
+def model3():
+    pirnt("Adding code of ML Model 3")
